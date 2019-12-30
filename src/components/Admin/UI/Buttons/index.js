7bc507d8f11ae3065674d6d0c30fde778243a923
liwenbin <liwenbin@apexsoft.com.cn>
2019-12-30 13:51:12 +0800

    less引用问题,需要后缀加上 module

--- src/components/Admin/UI/Buttons/index.js
+++ src/components/Admin/UI/Buttons/index.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { Card, Button, Row, Col } from 'antd';
+import styles from './index.module.less';
 
 class Footer extends Component {
   render() {
     return (
       <Row>
         <Col span={24}>
-          <Card title="基础按钮">
-            <Button type="primary">Primary</Button>
-            <Button>Normal</Button>
-            <Button type="dashed">Dashed</Button>
-            <Button type="danger">Danger</Button>
-            <Button type="link">Link</Button>
+          <Card title="基础按钮" className={styles.myCard}>
+            <div className={styles.btnList}>
+              <Button type="primary">Primary</Button>
+              <Button>Normal</Button>
+              <Button type="dashed">Dashed</Button>
+              <Button type="danger">Danger</Button>
+              <Button type="link">Link</Button>
+            </div>
+          </Card>
+        </Col>
+        <Col span={24}>
+          <Card title="基础按钮" className={styles.myCard}>
+            <div className={styles.btnList}>
+              <Button type="primary">Primary</Button>
+              <Button>Normal</Button>
+              <Button type="dashed">Dashed</Button>
+              <Button type="danger">Danger</Button>
+              <Button type="link">Link</Button>
+            </div>
           </Card>
         </Col>
       </Row>
